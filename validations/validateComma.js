aa82e822ec270af7ef179f6518972ce499c029d1
anto-christo <anto.christo.20@gmail.com>
2021-08-22 19:18:57 +0530

    fix: More suitable error message for missing comma

--- validations/validateComma.js
+++ validations/validateComma.js
@@ -12,7 +12,7 @@ const validateComma = (content, i)=> {
         throw new ValidationError({
             content,
             message: _.MISSING_CHARACTER,
-            expected: ',',
+            expected: ', before this key-value pair',
             position: i
         });
     }
